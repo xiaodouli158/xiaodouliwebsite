566259648f7eef5f3d5c281224791ed42e488019
xiaodouli158 <xiaodouli158@gmail.com>
2025-09-14 11:49:55 +0800

    router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { AppDataProvider } from './hooks/useAppData'
 import './style.css'
@@ -8,9 +8,9 @@ import './style.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppDataProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AppDataProvider>
   </StrictMode>,
 )
