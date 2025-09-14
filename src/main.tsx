eeeabc3ddea7e749c9174e6ae84451a49c9502a7
xiaodouli158 <xiaodouli158@gmail.com>
2025-09-14 11:32:48 +0800

    downloadfix4

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './style.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppDataProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </AppDataProvider>
